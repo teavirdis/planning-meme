153326b14487a8ce2c7bb357cdf103a28ac37b75
Raman Baranau <Raman_Baranau@epam.com>
2018-09-06 15:31:13 +0300

    test some story and vote services features

---
+++ src/test/java/com/epam/meme/service/impl/VoteServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.epam.meme.service.impl;
+
+import com.epam.meme.config.logic.ApplicationConfiguration;
+import com.epam.meme.service.VoteService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {ApplicationConfiguration.class})
+@Transactional
+public class VoteServiceImplTest {
+
+    @Autowired
+    private VoteService service;
+
+
+}
