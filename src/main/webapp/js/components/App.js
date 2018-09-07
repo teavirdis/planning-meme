d1fec4e1f33f1372985d3f62aae9cafd433878b0
Andrei_Dziamko <demkoandrey2012@gmail.com>
2018-09-07 13:13:30 +0300

    started impl react ui

---
+++ src/main/webapp/js/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import WelcomeForm from "./WelcomeForm";
+
+function App() {
+    return (
+        <div>
+            <h1> App name </h1>
+            <WelcomeForm/>
+        </div>
+    )
+}
+
+export default App
