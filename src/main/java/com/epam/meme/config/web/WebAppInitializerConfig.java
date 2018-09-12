09bda28f31be754b060ea2a8bb80d153633735d4
Andrei_Dziamko <demkoandrey2012@gmail.com>
2018-09-12 08:47:40 +0300

    added model mapper to convert entities to dto

--- src/main/java/com/epam/meme/config/web/WebAppInitializerConfig.java
+++ src/main/java/com/epam/meme/config/web/WebAppInitializerConfig.java
@@ -2,6 +2,7 @@ package com.epam.meme.config.web;
 
 import com.epam.meme.config.logic.ApplicationConfiguration;
 import com.thetransactioncompany.cors.CORSFilter;
+import io.swagger.jersey.config.JerseyJaxrsConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.springframework.core.annotation.Order;
 import org.springframework.lang.NonNullApi;
@@ -13,29 +14,26 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Order(1)
 public class WebAppInitializerConfig implements WebApplicationInitializer {
 
-    private static final String SERVLET_NAME = "PlanningServerApplication";
+    private static final String JERSEY_SERVLET_NAME = "PlanningServerApplication";
+    private static final String SWAGGER_SERVLET_NAME = "Jersey2Config";
     private static final String URL_PATTERN = "/meme/*";
     private static final String RS_APPLICATION = "javax.ws.rs.Application";
+
+    private static final String API_VERSION = "1.0.0";
+    private static final String SWAGGER_BASE_PATH = "http://localhost:8081/chat-server/meme";
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         servletContext.setInitParameter("contextConfigLocation", "");
         registerContextLoaderListener(servletContext);
+        registerJerseyServlet(servletContext);
 
-        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet(
-                SERVLET_NAME,
-                ServletContainer.class
-        );
-        servletRegistration.addMapping(URL_PATTERN);
-        servletRegistration.setLoadOnStartup(1);
-
-        servletRegistration.setInitParameter(
-                RS_APPLICATION,
-                MemeResourceConfiguration.class.getName());
     }
 
     private void registerContextLoaderListener(ServletContext servletContext) {
@@ -48,4 +46,30 @@ public class WebAppInitializerConfig implements WebApplicationInitializer {
         servletContext.addListener(new ContextLoaderListener(configWebApplicationContext));
     }
 
+    private void registerJerseyServlet(ServletContext servletContext) {
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet(
+                JERSEY_SERVLET_NAME,
+                ServletContainer.class
+        );
+        servletRegistration.addMapping(URL_PATTERN);
+        servletRegistration.setLoadOnStartup(1);
+
+        servletRegistration.setInitParameter(
+                RS_APPLICATION,
+                MemeResourceConfiguration.class.getName());
+    }
+
+    private void registerSwaggerServlet(ServletContext servletContext) {
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet(
+                SWAGGER_SERVLET_NAME,
+                JerseyJaxrsConfig.class
+        );
+        servletRegistration.setLoadOnStartup(2);
+
+        Map<String, String> swaggerInitParameters = new HashMap<>();
+        swaggerInitParameters.put("api.version",API_VERSION);
+        swaggerInitParameters.put("swagger.api.basepath",SWAGGER_BASE_PATH);
+
+        servletRegistration.setInitParameters(swaggerInitParameters);
+    }
 }
