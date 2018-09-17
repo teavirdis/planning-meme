16e807dd0678243930aa6bae61385ad3bb3fa97c
Andrei_Dziamko <demkoandrey2012@gmail.com>
2018-09-17 08:44:47 +0300

    edit ui script

---
+++ frontend/UIEditor.bat
@@ -0,0 +1,12 @@
+@echo off 
+setLocal EnableExtensions EnableDelayedExpansion
+set src=/static
+set var=../resources/static
+Set infile=frontend/build/index.html
+<"%infile%" >"TMP.TXT" (
+ for /f %%a in ('find /c /v "" ^< "%infile%"') do for /L %%c in (1,1,%%a) do (
+    set LINE=& set /p LINE=
+    if defined LINE (echo !LINE:%src%=%var%!) else echo.
+ )
+)
+move TMP.TXT "%infile%"
