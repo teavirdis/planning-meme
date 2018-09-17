bbec060a249e180066a576de9030ec9a1f13e1aa
Andrei_Dziamko <demkoandrey2012@gmail.com>
2018-09-17 14:43:32 +0300

    edit gradle scripts

--- frontend/UIEditor.bat
+++ frontend/UIEditor.bat
@@ -1,7 +1,7 @@
 @echo off 
 setLocal EnableExtensions EnableDelayedExpansion
 set src=/static
-set var=../resources/static
+set var=resources/static
 Set infile=frontend/build/index.html
 <"%infile%" >"TMP.TXT" (
  for /f %%a in ('find /c /v "" ^< "%infile%"') do for /L %%c in (1,1,%%a) do (
